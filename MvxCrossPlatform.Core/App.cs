b6dedb4fec439d61355861c2fbcfb426acf0d0fd
Mniwyl <wenyule@163.com>
2020-10-22 00:06:03 +0800

    mccmvross

---
+++ MvxCrossPlatform.Core/App.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using MvxCrossPlatform.Core.ViewModels;
+
+namespace MvxCrossPlatform.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<MainViewModel>();
+        }
+    }
+}
---
+++ MvxCrossPlatform.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.ViewModels;
+
+namespace MvxCrossPlatform.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+        private string _userName = "admin";
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+    }
+}
